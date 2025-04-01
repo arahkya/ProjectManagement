b298352f26e1f1361422694931ea77ff470c6352
Arahk Yambupah <arahk@outlook.com>
2025-04-01 21:17:21 +0700

    Add more Unit Tests

--- Arahk.ProjectManagement.WebApi.Tests/ProjectStatusUnitTest.cs
+++ Arahk.ProjectManagement.WebApi.Tests/ProjectStatusUnitTest.cs
@@ -1,11 +1,30 @@
 ﻿using Arahk.ProjectManagement.WebApi.Modules.Project.Entities;
 using Arahk.ProjectManagement.WebApi;
+using Arahk.ProjectManagement.WebApi.Modules.Project.Models;
 
 namespace Arahk.ProjectManagement.WebApi.Tests;
 
 public class ProjectStatusUnitTest(TestApplicationFactory<Program> factory) : IClassFixture<TestApplicationFactory<Program>>
 {
     private readonly TestApplicationFactory<Program> _factory = factory;
+
+    [Fact]
+    public async Task TestGetProjectStatusById()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/project/status/1");
+        var projectStatus = await response.Content.ReadFromJsonAsync<ProjectStatusEntity>();
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.NotNull(projectStatus);
+        Assert.Equal(1, projectStatus.Id);
+        Assert.Equal("Not Started", projectStatus.Name);
+        Assert.Equal(1, projectStatus.Order);
+    }
 
     [Fact]
     public async Task TestGetAllProjectStatus()
@@ -20,6 +39,78 @@ public class ProjectStatusUnitTest(TestApplicationFactory<Program> factory) : IC
         // Assert
         response.EnsureSuccessStatusCode();
         Assert.NotNull(projectStastuses);
-        Assert.Equal(5, projectStastuses.Count());
+        Assert.True(projectStastuses.Count() > 0);
+    }
+
+    [Fact]
+    public async Task TestDeleteProjectStatus()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var newProjectStatus = new CreateProjectStatusViewModel
+        {
+            Name = "Paused",
+            Order = 9
+        };
+
+        var response = await client.PostAsJsonAsync("/project/status", newProjectStatus);
+        var createdProjectStatus = await response.Content.ReadFromJsonAsync<ProjectStatusEntity>();
+
+        // Act
+        response = await client.DeleteAsync($"/project/status/{createdProjectStatus!.Id}");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+
+
+    [Fact]
+    public async Task TestCreateProjectStatus()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var newProjectStatus = new CreateProjectStatusViewModel
+        {
+            Name = "Paused",
+            Order = 9
+        };
+
+        // Act
+        var response = await client.PostAsJsonAsync("/project/status", newProjectStatus);
+        var createdProjectStatus = await response.Content.ReadFromJsonAsync<ProjectStatusEntity>();
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        Assert.NotNull(createdProjectStatus);
+        Assert.True(createdProjectStatus.Id > 0);
+    }
+
+    [Fact]
+    public async Task TestUpdateProjectStatus()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var newProjectStatus = new CreateProjectStatusViewModel
+        {
+            Name = "Paused",
+            Order = 9
+        };
+
+        var response = await client.PostAsJsonAsync("/project/status", newProjectStatus);
+        var createdProjectStatus = await response.Content.ReadFromJsonAsync<ProjectStatusEntity>();
+
+        // Act
+        var updateProjectStatus = new UpdateProjectStatusViewModel
+        {
+            Id = createdProjectStatus!.Id,
+            Name = "In Progress",
+            Order = 8
+        };
+
+        response = await client.PatchAsJsonAsync("/project/status", updateProjectStatus);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
     }
 }
--- Arahk.ProjectManagement.WebApi.Tests/ProjectUnitTest.cs
+++ Arahk.ProjectManagement.WebApi.Tests/ProjectUnitTest.cs
@@ -1,6 +1,8 @@
 ﻿using Arahk.ProjectManagement.WebApi.Modules.Project.Entities;
 using Arahk.ProjectManagement.WebApi;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Arahk.ProjectManagement.WebApi.Modules.Project.Models;
+using System.Net;
 
 namespace Arahk.ProjectManagement.WebApi.Tests;
 
@@ -22,5 +24,123 @@ public class ProjectUnitTest(TestApplicationFactory<Program> factory) : IClassFi
         response.EnsureSuccessStatusCode();
         Assert.NotNull(projects);
         Assert.Equal(15, projects.Count());
+    }
+
+    [Fact]
+    public async Task TestCreateProject()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var newProject = new CreateProjectViewModel
+        {
+            Name = "New Project",
+            Description = "This is a new project",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(30)
+        };
+
+        // Act
+        var response = await client.PostAsJsonAsync("/project", newProject);
+        var createdProject = await response.Content.ReadFromJsonAsync<ProjectEntity>();
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        Assert.NotNull(createdProject);
+        Assert.True(createdProject.Id > 0);
+        Assert.True(createdProject.StatusId > 0);
+        Assert.Equal("Not Started", createdProject.Status.Name);
+    }
+
+    [Fact]
+    public async Task TestUpdateProject()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var projectId = 1; // Assuming a project with ID 1 exists
+        var updatedProject = new UpdateProjectViewModel
+        {
+            Id = projectId,
+            Name = "Updated Project",
+            Description = "This is an updated project",
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(60),
+            StatusId = 2
+        };
+
+        // Act
+        var response = await client.PatchAsJsonAsync($"/project", updatedProject);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task TestDeleteProject()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var projectId = 1; // Assuming a project with ID 1 exists
+
+        // Act
+        var response = await client.DeleteAsync($"/project/{projectId}");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task TestGetProjectById()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var projectId = 2; // Assuming a project with ID 1 exists
+
+        // Act
+        var response = await client.GetAsync($"/project/{projectId}");
+        var project = await response.Content.ReadFromJsonAsync<ProjectEntity>();
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.NotNull(project);
+        Assert.Equal(projectId, project.Id);
+    }
+
+    [Fact]
+    public async Task TestGetAllProjects()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/project");
+        var projects = await response.Content.ReadFromJsonAsync<IEnumerable<ProjectEntity>>();
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.NotNull(projects);
+        Assert.Equal(15, projects.Count());
+    }
+
+    [Theory]
+    [InlineData("", "This is a new project")]
+    [InlineData("Another Project", "")]
+    public async Task TestCreateProjectWithInvalidContext(string name, string desc)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var newProject = new CreateProjectViewModel
+        {
+            Name = name,
+            Description = desc,
+            StartDate = DateTime.UtcNow,
+            EndDate = DateTime.UtcNow.AddDays(30)
+        };
+
+        // Act
+        var response = await client.PostAsJsonAsync("/project", newProject);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 }
